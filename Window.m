b4ee8f2207bfd5dcd6328032c4bce96375ddfde2
Jared Willets <61078848+JaredWillets@users.noreply.github.com>
2023-11-26 23:58:38 -0600

    Updated Window.m for proper orientation

--- Window.m
+++ Window.m
@@ -15,13 +15,15 @@ classdef Window
             
         end
         function this = blit(self, object, position)
-            self.pixels(position(1):position(1)+object.width-1, position(2):position(2)+object.height-1, 1) = self.pixels(position(1):position(1)+object.width-1, position(2):position(2)+object.height-1, 1) + double(object.pixels(:,:,1)); %.* object.pixels(:,:,4);
-            self.pixels(position(1):position(1)+object.width-1, position(2):position(2)+object.height-1, 2) = self.pixels(position(1):position(1)+object.width-1, position(2):position(2)+object.height-1, 2) + double(object.pixels(:,:,2)); %.* object.pixels(:,:,4);
-            self.pixels(position(1):position(1)+object.width-1, position(2):position(2)+object.height-1, 3) = self.pixels(position(1):position(1)+object.width-1, position(2):position(2)+object.height-1, 3) + double(object.pixels(:,:,3)); %.* object.pixels(:,:,4);
+            self.pixels(position(1):position(1)+object.width-1, position(2):position(2)+object.height-1, 1) = self.pixels(position(1):position(1)+object.width-1, position(2):position(2)+object.height-1, 1) + imrotate(double(object.pixels(:,:,1)),90,'bilinear','crop') .* object.transparency(:,:);
+            self.pixels(position(1):position(1)+object.width-1, position(2):position(2)+object.height-1, 2) = self.pixels(position(1):position(1)+object.width-1, position(2):position(2)+object.height-1, 2) + imrotate(double(object.pixels(:,:,2)),90,'bilinear','crop') .* object.transparency(:,:);
+            self.pixels(position(1):position(1)+object.width-1, position(2):position(2)+object.height-1, 3) = self.pixels(position(1):position(1)+object.width-1, position(2):position(2)+object.height-1, 3) + imrotate(double(object.pixels(:,:,3)),90,'bilinear','crop') .* object.transparency(:,:);
             this = self;
         end
         function render(self)
-            imshow(uint8(self.pixels(:,:,1:3)));
+            temp = imrotate(self.pixels,90,'bilinear','crop');
+            temp = flip(temp, 1);
+            imshow(uint8(temp(:,:,1:3)));
         end
         function this = clear(self)
             self.pixels = zeros(self.resolution);
